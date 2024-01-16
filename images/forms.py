50ce60ce2d616cbb46da9789257d26469221136c
CK Wong <alvinwong7896@gmail.com>
2024-01-16 15:08:11 +0800

    testing for bookmark

--- images/forms.py
+++ images/forms.py
@@ -11,28 +11,26 @@ class ImageCreateForm(forms.ModelForm):
         fields = ("title", "url", "description")
         widgets = {"url": forms.HiddenInput}
 
+    def clean_url(self):
+        url = self.cleaned_data.get("url")
+        valid_extensions = ["jpg", "jpeg"]
+        extension = url.rsplit(".", 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError(
+                "The given URL does not match valid image extensions."
+            )
+        return url
 
-def clean_url(self):
-    url = self.clean_field("url")
-    valid_extensions = ["jpg", "jpeg"]
-    extension = url.rsplit(".", 1)[1].lower()
-    if extension not in valid_extensions:
-        raise forms.ValidationError(
-            "The given URL does not match valid image extensions."
-        )
-    return
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data["url"]
+        name = slugify(image.title)
+        extension = image_url.rsplit(".", 1)[1].lower()
+        image_name = f"{name}.{extension}"
 
-
-def save(self, force_insert=False, force_update=False, commit=True):
-    image = super().save(commit=False)
-    image_url = self.cleaned_data("url")
-    name = slugify(image.title)
-    extension = image_url.rsplit(".", 1)[1].lower()
-    image_name = f"{name}.{extension}"
-
-    # download image from the given URL
-    response = request.urlopen(image_url)
-    image.image.save(image_name, ContentFile(response.read()), save=False)
-    if commit:
-        image.save()
-    return
+        # download image from the given URL
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+        if commit:
+            image.save()
+        return image
