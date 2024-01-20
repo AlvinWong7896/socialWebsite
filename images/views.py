0671e4991235c3e0581b5245488c1a2bf771db28
CK Wong <alvinwong7896@gmail.com>
2024-01-20 15:49:06 +0800

    completed social website, pending integrated testing

--- images/views.py
+++ images/views.py
@@ -1,5 +1,6 @@
 from actions.utils import create_action
 from bookmarks.common.decorators import ajax_required
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -8,6 +9,13 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from .forms import ImageCreateForm
 from .models import Image
+import redis
+
+
+# connect to redis
+r = redis.Redis(
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -39,8 +47,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # increment total image views by 1
+    total_views = r.incr(f"image:{image.id}:views")
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
     )
 
 
@@ -89,4 +101,20 @@ def image_list(request):
         )
     return render(
         request, "images/image/list.html", {"section": "images", "images": "images"}
+    )
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange("image_ranking", 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(id_in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(
+        request,
+        "images/image/ranking_html",
+        {"section": "images", "most_viewed": most_viewed},
     )
